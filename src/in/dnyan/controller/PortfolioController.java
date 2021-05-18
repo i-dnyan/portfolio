bc471b42035f43af8dc4b1bb19e60687c873a421
Dnyaneshwar Ingale <84164363+i-dnyan@users.noreply.github.com>
2021-05-18 07:13:43 +0530

    initial commit

---
+++ src/in/dnyan/controller/PortfolioController.java
@@ -0,0 +1,18 @@
+package in.dnyan.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PortfolioController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(PortfolioController.class);
+
+	@GetMapping("/")
+	public String rootContext() {
+		LOG.info("Inside rootContext");
+		return "home";
+	}
+}
